1bf1dadc988f16017944f6be8177a22c1503acbe
Raya Petkova <rayapetkova2007@abv.bg>
2025-10-14 01:18:27 +0300

    Add tasks 1-4 -> first exercise

---
+++ 1. First year/first_exercise/task_1.py
@@ -0,0 +1,5 @@
+first_num = float(input("Въведете първото число: "))
+second_num = float(input("Въведете второто число: "))
+
+result = first_num + second_num
+print(f"Резултат: {first_num} + {second_num} = {result}")
