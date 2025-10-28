5aefb85d1b68508a3b0bf48aa6c9f3173874b1bf
Raya Petkova <rayapetkova2007@abv.bg>
2025-10-28 12:10:28 +0200

    Fix tasks

--- 1. First year/seminar_exercises/first_exercise/task_1.py
+++ 1. First year/seminar_exercises/first_exercise/task_1.py
@@ -4,7 +4,7 @@ n = int(input())
 final_result = 1
 
 for num in range(m, n + 1):
-    if num % 3 == 0 or num % 4 == 0:
+    if num % 3 == 0 and num % 4 == 0:
         final_result *= num
 
 print(final_result)
